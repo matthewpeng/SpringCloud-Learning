5d3fd7d14f3a7d3bbf519a694b2755ec1dd06afc
matthew <68773479@qq.com>
2020-04-20 06:21:27 +0800

    nacos 实现动态路由 有修改pom文件版本

--- cloudalibaba-config-nacos-client3377/src/main/java/com/matthew/practice/NacosConfigClientMain3377.java
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/matthew/practice/NacosConfigClientMain3377.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NacosConfigClientMain3377 {
+    //http://localhost:3377/actuator/gateway/routes 查看路由列表
     public static void main(String[] args) {
         SpringApplication.run(NacosConfigClientMain3377.class, args);
     }
